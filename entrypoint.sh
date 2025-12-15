da8433505d11115012dd7665df0398e1d5bcb1be
zhaocheng <zhaocheng990105@gmail.com>
2025-12-15 11:05:10 +0800

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,35 @@
 #!/bin/sh
 set -e
 
-create_directories() {
-    [ ! -d '/danmuji/Danmuji_log' ] && mkdir /danmuji/Danmuji_log
-    [ ! -d '/danmuji/guardFile' ] && mkdir /danmuji/guardFile
-    [ ! -d '/danmuji/log' ] && mkdir /danmuji/log
-}
+WORKDIR=/danmuji
+DANMUJI_LOG=/danmuji/Danmuji_log
+GUARDFILE=/danmuji/guardFile
+LOG=/danmuji/log
 
-# 使用传入的 UID 和 GID 创建用户
-if [ -n "$UID" ] && [ -n "$GID" ]; then
-    groupadd -g "$GID" danmuji
-    useradd -u "$UID" -g "$GID" -m danmuji
-    chown -R danmuji:danmuji /danmuji
+UMASK=${UMASK:-022}
+PUID=${PUID:-0}
+PGID=${PGID:-0}
 
-    # 以指定的 UID 和 GID 创建文件夹
-    su-exec danmuji:danmuji sh -c "$(declare -f create_directories); create_directories"
-    
-    # 以指定的 UID 和 GID 启动程序
-    exec su-exec danmuji:danmuji sh -c "java ${JAVA_OPTS} -jar danmuji.jar ${JAVA_OPTS2}"
-else
-    # 如果没有指定 UID 和 GID，默认创建文件夹并启动程序
-    create_directories
-    exec sh -c "java ${JAVA_OPTS} -jar danmuji.jar ${JAVA_OPTS2}"
+umask ${UMASK}
+echo "Starting Danmuji with PUID:PGID ${PUID}:${PGID} and UMASK ${UMASK}"
+
+# 检查并创建用户（如果需要）
+if [ -n "$PUID" ] && [ "$PUID" -ne 0 ]; then
+    echo "Creating user with PUID:$PUID and PGID:$PGID"
+    if ! getent group "$PGID" >/dev/null 2>&1; then
+        groupadd -g "$PGID" danmuji
+    fi
+    if ! getent passwd "$PUID" >/dev/null 2>&1; then
+        useradd -u "$PUID" -g "$PGID" -d $WORKDIR -s /bin/sh danmuji
+    fi
 fi
+
+mkdir -p ${DANMUJI_LOG} ${GUARDFILE} ${LOG}
+echo "Ensured directories ${DANMUJI_LOG} ${GUARDFILE} ${LOG} exist"
+
+#设置权限
+chown -R ${PUID}:${PGID} ${DANMUJI_LOG} ${GUARDFILE} ${LOG}
+chmod -R 755 ${DANMUJI_LOG} ${GUARDFILE} ${LOG}
+echo "Set permissions for ${DANMUJI_LOG} ${GUARDFILE} ${LOG}"
+
+exec sudo -u "#$PUID" -g "#$PGID" java ${JAVA_OPTS} -jar danmuji.jar ${JAVA_OPTS2}
